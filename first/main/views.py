b34de7c3d8c9f6cec605a94862821c3e357b282e
OWLsTyLe <maxshypilkin@gmail.com>
2025-01-06 15:34:17 +0200

    test

--- first/main/views.py
+++ first/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h4> hello world</h4>')
+    return HttpResponse('<h4> hello world</h4>sqd')
