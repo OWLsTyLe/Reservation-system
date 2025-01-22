91478b23222d432ad8dbf76fd5ebf744be6f1079
OWLsTyLe <maxshypilkin@gmail.com>
2025-01-22 14:38:59 +0200

    new urls

--- first/main/urls.py
+++ first/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('ackount', views.ackount, name='ackount')
 ]
--- first/main/views.py
+++ first/main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def ackount(request):
+    return render(request, 'main/ackount.html')
