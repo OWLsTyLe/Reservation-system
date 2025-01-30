1fb00dece7e64fdf638a866a95fb27723b07b755
OWLsTyLe <maxshypilkin@gmail.com>
2025-01-30 15:43:17 +0200

    update

--- first/restourants/urls.py
+++ first/restourants/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.restaurant_list, name='restourants'),
-    path('<int:pk>', views.RestaurantDetailView.as_view(), name='restaurant')
+    path('<int:pk>', views.RestaurantDetailView.as_view(), name='restaurant'),
+    path("reservation/<int:restaurant_id>/",  views.reservation_view, name="reservation"),
+    path("reservation/success/", views.reservation_success, name="reservation_success"),
 ]
--- first/restourants/views.py
+++ first/restourants/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Restaurant
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Restaurant, Table, Reservation
+from .forms import ReservationForm
+from django.contrib.auth.decorators import login_required
 from django.views.generic import DetailView
 
 def restaurant_list(request):
@@ -10,3 +12,23 @@ class RestaurantDetailView(DetailView):
     model = Restaurant
     template_name = 'restourants/restaurant.html'
     context_object_name = 'restaurant'
+
+
+@login_required
+def reservation_view(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    if request.method == 'POST':
+        form = ReservationForm(request.POST, restaurant_id = restaurant_id)
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            reservation.user = request.user
+            reservation.save()
+            return redirect('reservation_success')
+
+    else:
+        form = ReservationForm(restaurant_id=restaurant_id)
+
+    return render(request, 'restourants/restaurant.html', {'form': form, "restaurant": restaurant})
+
+def reservation_success(request):
+    return render(request, "main/index.html")
