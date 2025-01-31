2e82b635b9932dbf5503b847804ae610c22bfb22
OWLsTyLe <maxshypilkin@gmail.com>
2025-01-31 16:01:59 +0200

    add email api

--- first/booking/views.py
+++ first/booking/views.py
@@ -3,6 +3,27 @@ from django.contrib.auth.decorators import login_required
 from .forms import BookingForm
 from hotels.models import HotelRoom
 from .models import Booking
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def send_booking_confirmation(user_email, booking):
+    subject = "Підтвердження бронювання"
+    message = f"""
+    Вітаємо, {booking.user.username}!
+
+    Ваше бронювання успішно створено.
+
+    Готель: {booking.room.hotel.name}
+    Тип номеру: {booking.room.room_type.name}
+    Ціна за ніч: {booking.room.price_per_night} грн
+    Дата заїзду: {booking.start_date}
+    Дата виїзду: {booking.end_date}
+
+    Дякуємо, що обрали наш сервіс!
+    """
+
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [user_email])
 
 @login_required
 def create_booking(request):
@@ -17,37 +38,19 @@ def create_booking(request):
                 room.available_count -= 1
                 room.save()
 
-                Booking.objects.create(
+                booking = Booking.objects.create(
                     user=request.user,
                     room=room,
                     start_date=start_date,
                     end_date=end_date
                 )
-                return redirect('home')
+
+                send_booking_confirmation(request.user.email, booking)
+                return redirect('ackount')
             else:
                 form.add_error('room', 'The selected room is no longer available.')
     else:
         form = BookingForm()
 
     return render(request, 'booking/create_booking.html', {'form': form})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- first/first/settings.py
+++ first/first/settings.py
@@ -131,3 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'maxshypilkin@gmail.com'
+EMAIL_HOST_PASSWORD = 'qjxr vtoq dclg mwjy'
